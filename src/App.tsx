2e24f8f82f449cbf821f6dae932f3b9bcc1a109d
tak.song-at-585251656769 <>
2024-01-12 19:04:30 +0900

    해시라우터로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 import _ from "lodash";
 
 import MainRouter from "./MainRouter";
@@ -11,17 +13,17 @@ import "./Mobile.css";
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         {/* <ScrollTop /> */}
         <Routes>
           {/* <Route path="/*" element={<MainRouter />} /> */}
-          <Route path="/*" element={<StampPage />} />
-          <Route path="/" />
+          {/* <Route path="/*" element={<StampPage />} /> */}
+          {/* <Route path="/" /> */}
           <Route path="/main" element={<MainPage />} />
           <Route path="/saju" element={<SajuPage />} />
           <Route path="/remaining-days" element={<RemainDaysPage />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
       {/* <SajuPage /> */}
     </div>
   );
